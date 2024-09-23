33cbdaac6590702c911ca21ffe5d03b319a5195b
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2024-09-23 08:23:37 +0100

    card

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,33 +6,41 @@ import GithubIcon from "./icons/GithubIcon";
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
-      <Image src={project.image} height={300} width={600} alt={project.name} />
+      <Image
+        className={styles.img}
+        src={project.image}
+        height={300}
+        width={600}
+        alt={project.name}
+      />
       <div className={styles.content}>
-        <h3>
-          <a
-            href={project.demo}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.underline}
-          >
-            {project.name}
-          </a>{" "}
-          <a
-            target={project.name}
-            className="github"
-            href={String(project.source_code)}
-          >
-            <GithubIcon fill={"rgb(106, 115, 125)"} className={styles.icon} />
-          </a>
-        </h3>
-        <p>{project.description}</p>
+        <div>
+          <h3>
+            <a
+              href={project.demo}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.underline}
+            >
+              {project.name}
+            </a>{" "}
+            <a
+              target={project.name}
+              className="github"
+              href={String(project.source_code)}
+            >
+              <GithubIcon fill={"rgb(106, 115, 125)"} className={styles.icon} />
+            </a>
+          </h3>
+          <p>{project.description}</p>
 
-        <div className={styles.tags}>
-          {project.tags.map((tag) => (
-            <span key={tag} className={tag}>
-              {tag}
-            </span>
-          ))}
+          <div className={styles.tags}>
+            {project.tags.map((tag) => (
+              <span key={tag} className={tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
